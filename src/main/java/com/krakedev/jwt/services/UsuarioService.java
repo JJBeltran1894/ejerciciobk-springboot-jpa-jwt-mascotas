1e778c9ccd9250446680e5b093755124127c4a1e
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-10 03:42:05 -0500

    JJBA: En la clase servicio UsuarioService se agregan la funcionalidad de encriptacion de contraseña con BCrypt antes de guardar en la BD y asi tambien la validacion dentro del login

--- src/main/java/com/krakedev/jwt/services/UsuarioService.java
+++ src/main/java/com/krakedev/jwt/services/UsuarioService.java
@@ -2,6 +2,7 @@ package com.krakedev.jwt.services;
 
 import java.util.Optional;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import com.krakedev.jwt.entidades.Usuario;
@@ -13,10 +14,13 @@ public class UsuarioService {
 	private UsuarioRepository usuarioRepositorio;
 	
 	public UsuarioService(UsuarioRepository usuarioRepositorio) {
+		
 		this.usuarioRepositorio = usuarioRepositorio;
 	}
 	
 	public Usuario registrar(Usuario usuario) {
+		String passwordEncriptada = BCrypt.hashpw(usuario.getPassword(), BCrypt.gensalt());
+		usuario.setPassword(passwordEncriptada);
 		return usuarioRepositorio.save(usuario);
 	}
 	
@@ -25,10 +29,10 @@ public class UsuarioService {
 		
 		if(usuario.isPresent()) {
 			Usuario u = usuario.get();
-			if(u.getPassword().equals(password)) {
+			Boolean passValida = BCrypt.checkpw(password,u.getPassword());
+			if(passValida) {
 				return u;
 			}
-			
 		}
 		return null;
 	}
