da74c3e9638d0ae32e50ab765ea33fa8468b01df
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-10 03:44:30 -0500

    JJBA: En la clase controller AuthController se actualiza la logica para la gestion de los token en el metodo login, asi como el nuevo metodo perfil para validar el mismo

--- src/main/java/com/krakedev/jwt/controllers/AuthController.java
+++ src/main/java/com/krakedev/jwt/controllers/AuthController.java
@@ -1,14 +1,20 @@
 package com.krakedev.jwt.controllers;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.krakedev.jwt.entidades.Usuario;
 import com.krakedev.jwt.services.UsuarioService;
+import com.krakedev.jwt.utils.JwtUtil;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -34,14 +40,60 @@ public class AuthController {
 	public ResponseEntity<?> login(@RequestBody Usuario request){
 		try {
 			Usuario usuario = usuarioService.login(request.getUsername(),request.getPassword());
-			return ResponseEntity.ok("Login existoso. Bienvenido, "+usuario.getUsername());
+			if(usuario != null) {
+				String token = JwtUtil.generarToken(usuario.getUsername(), usuario.getRol());
+				//return ResponseEntity.ok("Login existoso. Bienvenido, "+usuario.getUsername());
+				return ResponseEntity.ok("token: "+token);
+			}else{
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
+			}
+
 		} catch (Exception e ) {
-			return ResponseEntity.status(401).body(e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
 			
 		}
 	}
-
 	
-	
+	@GetMapping("/perfil")
+	public ResponseEntity<?> perfil(@RequestHeader(value="Authorization", required = false) String authHeader){
+		try{
+			if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+						.body("Acceso Denegado: Debe proveer un token Bearer valido en la cebecera Authorization");
+			}
+			String token = authHeader.substring(7);
+			
+			DecodedJWT datosToken = JwtUtil.validarToken(token);
+			
+			if(datosToken ==null) {
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Acceso Denegado: Token invalido o expirado" );
+			}
+			
+			String usuario = datosToken.getSubject();
+			String rol = datosToken.getClaim("rol").asString();
+			
+			if ("ADMIN".equals(rol)) {
+				return ResponseEntity.ok(Map.of(
+						"Mensaje", "Bienvenido al sistema del refugio de mascotas", 
+						"Usuario", usuario,
+						"Rol", rol,
+						"Estatus", "Autenticado Existosamente"
+						));	
+			}else if("USER".equals(rol)){
+				return ResponseEntity.ok(Map.of(
+						"Mensaje", "Bienvenido al sistema del refugio de mascotas", 
+						"Usuario", usuario,
+						"Rol", rol,
+						"Estatus", "Autenticado Existosamente"
+						));	
+			}else {
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Rol no autorizado a acceder a este modulo");
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token invalido o expirado");
+		}
+			
+			
+	}
 
 }
